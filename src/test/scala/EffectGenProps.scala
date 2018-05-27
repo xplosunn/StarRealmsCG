2fcb63f4a49c93049467517dec7c61d4c2fcefe5
Hugo Sousa <hugo@codacy.com>
2018-05-27 23:21:44 +0100

    Test refactor

--- src/test/scala/EffectGenProps.scala
+++ src/test/scala/EffectGenProps.scala
@@ -1,18 +1,14 @@
+import EffectGenProps.{findGenEffect, property}
 import org.scalacheck.Prop.forAll
 import org.scalacheck.{Gen, Properties}
 
-object EffectGenProps extends Properties("EffectGen") {
+object EffectGenProps extends Properties("EffectGen") with CostProps {
 
   val effectGen = for {
     faction <- Gen.oneOf(Faction.values.toList)
     cost <- Gen.oneOf(1 to 8)
   } yield EffectGen(faction, cost)
-
-  val effectGenWithCost = for {
-    faction <- Gen.oneOf(Faction.values.toList)
-    cost <- Gen.oneOf(1 to 8)
-  } yield (EffectGen(faction, cost), cost)
-
+  
   property("nonEmpty") = forAll(effectGen) { effectList =>
     effectList.nonEmpty
   }
@@ -28,13 +24,6 @@ object EffectGenProps extends Properties("EffectGen") {
     }
   }
 
-  property("max stack holds") = forAll(effectGenWithCost) { case (effectList, cost) =>
-    effectList.forall {
-      case (effect, amount) =>
-        amount <= findGenEffect(effect).maxStack(cost)
-    }
-  }
-
   property("ships must have at least one common effect") = forAll(effectGen) { effectList =>
     effectList.exists {
       case (effect, _) =>
@@ -46,4 +35,18 @@ object EffectGenProps extends Properties("EffectGen") {
   def findGenEffect(effect: Effect): GenEffect =
     EffectGen.allEffects.filter(_.effect == effect).head
 
+}
+
+trait CostProps {
+  private val effectGenWithCost = for {
+    faction <- Gen.oneOf(Faction.values.toList)
+    cost <- Gen.oneOf(1 to 8)
+  } yield (EffectGen(faction, cost), cost)
+
+  property("max stack holds") = forAll(effectGenWithCost) { case (effectList, cost) =>
+    effectList.forall {
+      case (effect, amount) =>
+        amount <= findGenEffect(effect).maxStack(cost)
+    }
+  }
 }
