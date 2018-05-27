2a83077fc822508dfae50460e5cee97f044f8a40
Hugo Sousa <hugo@codacy.com>
2018-05-27 20:35:22 +0100

    Add faction-specific effects

--- src/main/scala/EffectGen.scala
+++ src/main/scala/EffectGen.scala
@@ -1,3 +1,4 @@
+import Faction.{Blob, MachineCult, StarEmpire, TradeFederation}
 import Ops._
 
 case class GenEffect(probability: Int,
@@ -5,27 +6,22 @@ case class GenEffect(probability: Int,
                      effect: Effect,
                      maxStack: Int => Int)
 
-object EffectGen {
-  private val dmgMaxStack: Int => Int = {
-    case 1 => 3
-    case 2 => 4
-    case 3 => 6
-    case 4 => 6
-    case 5 => 6
-    case 6 => 8
-    case 7 => 8
-    case 8 => 9
+object EffectGen
+  extends CommonEffects
+    with TradeFederationEffects
+    with MachineCultEffects
+    with BlobEffects
+    with StarEmpireEffects {
+
+  private def factionEffects(faction: Faction) = faction match {
+    case TradeFederation => tradeFederationEffects
+    case MachineCult => machineCultEffects
+    case Blob => blobEffects
+    case StarEmpire => starEmpireffects
   }
 
-  val commonEffects = List(
-    GenEffect(10, 0.35, Damage, dmgMaxStack),
-    GenEffect(10, 1, Gold, _ => 4),
-    GenEffect(2, 2.3, Draw, _ => 2),
-    GenEffect(1, 2.3, DestroyBase, _ => 1)
-  )
-
   def apply(faction: Faction, cost: Int): List[(Effect, Int)] =
-    effectGen(cost, commonEffects)
+    effectGen(cost, commonEffects ++ factionEffects(faction))
 
   private def effectGen(cost: Int, genEffects: List[GenEffect], maxEffects: Int = 3): List[(Effect, Int)] = {
     val costToSpendOpt = if(cost > 0) Some((1 to cost).toNEL().random) else None
@@ -47,3 +43,49 @@ object EffectGen {
   }
 }
 
+trait CommonEffects {
+  private val dmgMaxStack: Int => Int = {
+    case 1 => 3
+    case 2 => 4
+    case 3 => 6
+    case 4 => 6
+    case 5 => 6
+    case 6 => 8
+    case 7 => 8
+    case 8 => 9
+  }
+
+  val commonEffects = List(
+    GenEffect(10, 0.35, Damage, dmgMaxStack),
+    GenEffect(10, 1, Gold, _ => 4),
+    GenEffect(2, 2.3, Draw, _ => 2),
+    GenEffect(1, 2.3, DestroyBase, _ => 1)
+  )
+}
+
+trait TradeFederationEffects {
+  val tradeFederationEffects = List(
+    GenEffect(0, 0.5, LifeGain, _ => 6),
+    GenEffect(0, 1, NextAquiredToTop, _ => 1)
+  )
+}
+
+trait MachineCultEffects {
+  val machineCultEffects = List(
+    GenEffect(0, 0.7, Scrap, _ => 2)
+  )
+}
+
+trait BlobEffects {
+  val blobEffects = List(
+    GenEffect(0, 0.6, ScrapInTradeRow, _ => 2),
+    GenEffect(0, 0.9, AcquireForFreeUpTo, _ => 6)
+  )
+}
+
+trait StarEmpireEffects {
+  val starEmpireffects = List(
+    GenEffect(0, 0.7, OpponentDiscard, _ => 1),
+    GenEffect(0, 2, DiscardAndDraw, _ => 2)
+  )
+}
